1b99755b1dbe956816e8062da1e158c5304ec501
Alexa1320 <zalexa02613@gmail.com>
2026-07-27 00:02:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+require("dotenv").config();
+
+const {
+  Client,
+  GatewayIntentBits,
+  Events,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle
+} = require("discord.js");
+
+const axios = require("axios");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once(Events.ClientReady, () => {
+  console.log(`✅ Bot conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
